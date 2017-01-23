8b473b4803ff232df656cf3938b808e13f8b4ec9
szabbenjamin <szabbenjamin@gmail.com>
2017-01-23 17:51:36 +0100

    osmc full installer

---
+++ osmc_installer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+apt-get update;
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs git
+git clone https://github.com/szabbenjamin/ittotttv
+cd ittotttv/engine
+
+echo "#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+cd /home/osmc/ittotttv/engine
+npm start" > tv_servlet.sh
+chmod +x tv_servlet.sh
+
+npm install
+cd ..
+cp config.js.sample config.js
+echo "Add meg bejelentkezesi adataidat..."
+sleep 5
+nano config.js
+echo "[Unit]
+Description=Ittott.tv servlet app
+
+[Service]
+ExecStart=/home/osmc/ittotttv/engine/tv_servlet.sh
+Restart=always
+User=root
+Group=root
+Environment=PATH=/usr/bin:/usr/local/bin
+Environment=NODE_ENV=production
+WorkingDirectory=/home/osmc/ittotttv/engine
+
+[Install]
+WantedBy=multi-user.target" > ittotttv.service
+
+cp ittotttv.service /etc/systemd/system
+systemctl start ittotttv
+systemctl enable ittotttv
+
+echo "deb http://apt.osmc.tv krypton main" >> /etc/apt/sources.list
+apt-get update
+apt-get -y dist-upgrade && reboot
